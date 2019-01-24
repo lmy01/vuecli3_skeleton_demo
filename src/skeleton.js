009e787336fc8feed6faaea8ce71cf8c4d1d95de
Mengyu Li <mengyu.li@singlera.com>
2019-01-24 18:01:52 +0800

    a simple demo of skeleton

---
+++ src/skeleton.js
@@ -0,0 +1,10 @@
+// - Skeleton.js
+import Vue from 'vue';
+import Skeleton from './components/Skeleton.vue';
+
+export default new Vue({
+  components: {
+    Skeleton,
+  },
+  render: h => h(Skeleton),
+});
